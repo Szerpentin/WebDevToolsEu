10042d27fae7dde61b70d8afed8bc38c0a16b1c2
Bollobás Dávid <david.bollobas@gmail.com>
2021-03-15 20:35:34 +0100

    Add project files.

---
+++ WebDevTools.Frontend/Program.cs
@@ -0,0 +1,26 @@
+using Blazor.Analytics;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDevTools.Frontend
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddGoogleAnalytics("G-HEJT06HQG2");
+            await builder.Build().RunAsync();
+        }
+    }
+}
