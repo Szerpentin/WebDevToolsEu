371925c0ae52c318ecc8f8781c5e858e5bd0d90d
Bollobás Dávid <david.bollobas@gmail.com>
2021-03-17 10:50:30 +0100

    Image resizer done

--- WebDevTools.Frontend/Program.cs
+++ WebDevTools.Frontend/Program.cs
@@ -1,10 +1,12 @@
 using Blazor.Analytics;
+using BlazorDownloadFile;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using WebDevTools.Frontend.BL;
+
 
 namespace WebDevTools.Frontend
 {
@@ -14,7 +16,7 @@ namespace WebDevTools.Frontend
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
-
+            builder.Services.AddBlazorDownloadFile();
             builder.Services.AddGoogleAnalytics("G-HEJT06HQG2");
             builder.Services.AddScoped<ClipboardService>();
             await builder.Build().RunAsync();
